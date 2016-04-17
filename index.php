499b43c73b89a71d60c1733a6c5702e301dfab4d
Vianney Cuvelier <vianneycuvelier97@gmail.com>
2016-04-17 19:28:26 +0200

    Upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Dossier: League Of Legends</title>
+	<link rel="stylesheet/less" type="text/css" href="assets/less/style.less"/>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
+</head>
+<body>
+	
+	<section class="header">
+		<div class="header--title">Dossier</div>
+		<div class="header--menu">
+			<a href="#">Intro</a> | <a href="#goal">1</a> | <a href="#player">2</a> | <a href="#champ">3</a> | <a href="#around">4</a>
+		</div>
+	</section>
+	
+
+	<section class="intro" id="intro">
+		<div class="presentation"> 
+			<h1>DOSSIER : League of Legends</h1> 
+		</div>
+		<div class="content">
+			<h3>League of Legends, c'est quoi ?</h3>
+			<p>League of Legends (parfois abrégé LoL) est un jeu vidéo d'ordinateur de type arène de bataille en ligne multijoueur (MOBA) gratuit sorti en 2009 et développé et édité par Riot Games sur Windows et Mac OS X.
+			Depuis, League of Legends est devenu rapidement un véritable phénomène. C'est en 2013 qu'il devient l'un des jeux les plus joués au monde. Il séduit aussi le monde du sport électronique en devenant l'événement eSport le plus regardé de l'histoire, avec plus de 32 millions de spectateurs en ligne et plus de 8,5 millions de vues simultanées lors de la finale de la saison 3. Le record est de nouveau battu lors de la saison 5 avec un total de 36 millions de spectateurs avec un pic d'audience de 14 millions de spectateur.</p>
+		</div>
+	</section>
+
+
+	<section class="goal" id="goal">
+		<div class="presentation">
+			<h2>1. Le but du jeu</h2> 
+		</div>
+		<div class="content">
+			<h3>Comment se déroule une partie ?</h3>
+			<p>	Le jeu se décompose en sessions, avec deux équipes qui s'affrontent par le biais de champions invoqués par les joueurs, lesquels incarnent des invocateurs. Chaque équipe comprenant 3 ou 5 joueurs en fonction du mode de jeu, a pour but de détruire le “nexus”, le batiment principale de l’équipe adverse. Pour cela, chaque équipe devra avancer sur une carte, en détruisant tourelle par tourelle pour avancer à l’aide d’une armée de petit monstre. Il sera également possible de tirer avantage de la carte, notamment dans la “jungle” ou la plupart des monstres nous offrent des bonus et avantages lors de leur mort.
+			</p>
+			<p>
+			Au début de chaque bataille, chacun des joueurs peut choisir un champion qui va le représenter au combat. Il peut choisir parmi plus de cents champions et acheter son équipement parmi une liste de 200 objets uniques pour créer des combinaisons efficaces au combat: objets de base (potions, élixirs ou balises de vision), équipements classiques (caractéristiques ou capacités de combat), et objets légendaires, créés en combinant plusieurs équipements. Une grande partie de ces objets confèrent des effets passifs ou actifs souvent plus complexes qu'une simple augmentation de statistiques (ralentissement des ennemis touchés par un sort, saignement, révélation d'une partie de la carte, etc.). De multiples stratégies sont donc possibles au cours d’une partie.</p>
+		</div>
+	</section>
+
+
+	<section class="player" id="player">
+		<div class="presentation">
+			<h2>2. Le joueur</h2>
+		</div>
+		<div class="content">
+			
+		</div>
+	</section>
+
+
+	<section class="champ" id="champ">
+		<div class="presentation">
+			<h2>3. Les champions</h2>
+		</div>
+		<div class="content">
+			
+		</div>
+	</section>
+
+
+	<section class="around" id="around">
+		<div class="presentation">
+			<h2>4. Autour du jeu</h2>
+		</div>
+		<div class="content">
+			
+		</div>
+	</section>
+
+
+	<section class="footer">
+		
+	</section>
+
+</body>
+</html>
