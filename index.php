b87fbcc43c20c2254af44fb4264b4fa35ce674f9
Vianney CUVELIER <vianneycuvelier97@gmail.com>
2016-04-18 00:01:19 +0200

    Projet final - Rendu

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Dossier: League Of Legends</title>
 	<link rel="stylesheet/less" type="text/css" href="assets/less/style.less"/>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	
